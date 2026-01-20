5d05e917bbe94993c7a06a5de96d54568af54794
GoldTitaniumSulfurCarbon <95988349+GoldTitaniumSulfurCarbon@users.noreply.github.com>
2026-01-19 20:10:20 -0600

    Update main.py
    Annotations

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from effects.text_watermark import TextWatermarkParamaters, TextWatermarkEffect
 from effects.image_watermark import ImageWatermarkParamaters, ImageWatermarkEffect
 
 
-def testing_text_time(): #Testing if each of the 6 scenarios I want do in fact work.
+def testing_text_time(): #Demonstration with relative path.
     fox = ClipEditor("video_2025-05-25_02-02-44.mp4")
     fox_params = TextWatermarkParamaters("Cute boy", end_time = 3)
     fox_image = ImageWatermarkParamaters("E:\Pictures/500px-Fisheyejak_original.jpg", position=("right","top"))
@@ -13,11 +13,9 @@ def testing_text_time(): #Testing if each of the 6 scenarios I want do in fact w
 
 #testing_text_time()
 
-def FAFO():
-    FAFO = ClipEditor("E:\PICTURES\libtard_FAFO.mp4").crop(12,17)
+def FAFO(): #Demonstration with absolute path for ClipEditor
+    FAFO = ClipEditor("E:\PICTURES\FAFO.mp4").crop(12,17)
     FAFO_half = FAFO.duplicate().speed(.5)
     FAFO.concatenate(FAFO_half)
-
-
     FAFO.preview()
 FAFO()
